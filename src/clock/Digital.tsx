158028fe522f99ef650ee41a968f444cd49ee747
Nicolas Alonso <Nicolas.Alonso@ventionteams.com>
2025-09-25 16:03:11 +0300

    fix: padding digital clock with 0s to 2 digits

--- src/clock/Digital.tsx
+++ src/clock/Digital.tsx
@@ -3,5 +3,9 @@ type DigitalProps = {
 }
 
 export function Digital({ time }: DigitalProps) {
-    return <h2>Digital: {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}</h2>;
+    const hours = String(time.getHours()).padStart(2, '0');
+    const minutes = String(time.getMinutes()).padStart(2, '0');
+    const seconds = String(time.getSeconds()).padStart(2, '0');
+    
+    return <h2>Digital: {hours}:{minutes}:{seconds}</h2>;
 }
